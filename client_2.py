855be84d72bbdf656281057df73391ce3d6a94df
DK404 <DK404.main@gmail.com>
2024-12-03 01:43:40 +0200

    Added a database system and resolved the last issue (which was blocking multithreading).
    Resolved a past problem that the script-server could not handle two or more clients at the same time, this was solved by fixing the use of the “threading” module.
    Also created a database system and linked to the script-server and script-client (separately, the server part of the database is linked to the script-server, and the client part - to the script-client).
    The unnecessary for-else and if-else blocks are removed due to the use of the “threading” module.

--- client_2.py
+++ client_2.py
@@ -1,18 +1,38 @@
 import socket
 from random import randint
+import sqlite3
+import time
 
+database = sqlite3.connect("user_info.db")
+cur = database.cursor()
+database.execute("CREATE TABLE if not exists User(username TEXT, user_tag TEXT, user_id INTEGER, user_mail TEXT, chat_now TEXT, chat_list TEXT, creation_date TEXT);")
 client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ("localhost", 38385)
-id_user = ''.join([*map(str, [randint(0,9) for _ in range(0,10)])])
-user = [id_user]
+
 chats = []
 chat_now = None
+cur.execute("SELECT * FROM User")
+info_user = cur.fetchone()
 
+if info_user == None:
+    print("REGISTRATION USER:")
+    username = input("Username: ")
+    user_tag = input("Tag: ")
+    user_mail = input("Mail: ")
+    user_password = input("Password: ")
+    user_id = ''.join([*map(str, [randint(0,9) for _ in range(0,10)])])
+    time_now = time.localtime()
+    time_structurized = f"{time_now.tm_hour}:{time_now.tm_min}:{time_now.tm_sec}|{time_now.tm_mday}.{time_now.tm_mon}.{time_now.tm_year}"
+    database.execute(f"INSERT INTO User(username, user_tag, user_id, user_mail, chat_now, chat_list, creation_date) VALUES('{username}', '{user_tag}', '{user_id}', '{user_mail}', 'None', '', '{time_structurized}');") #%(username, user_tag, user_id, user_mail, time_structurized)
+    database.commit()
 while 1:
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.connect(server_address)
-            sock.sendall(user[0].encode())
+            cur.execute("SELECT * FROM User;")
+            info = cur.fetchone()
+            info = "↔".join([*map(str, info)])
+            sock.sendall(f"{info}".encode())
             print(f"Connected to server({server_address})")
             while 1:
                 if chat_now!=None:
@@ -20,14 +40,16 @@ while 1:
                     sock.sendto(message, server_address)
                     print(f"Message '{message.decode()}' are sent.")
                 else:
-                    code, chats = sock.recv(10024).decode().split("#")
+                    chats = sock.recv(10024).decode()
                     exec(f"chats={chats}")
+                    chats = [*{*chats}]
                     for chat in chats:
-                        print(f"----------------\n|Chat name: {chat['chat_name']}\n|ID-chat: {chat['chat_id']}\n|Chat tag: {chat['chat_tag']}\n----------------")
+                        print(f"----------------\n|Chat name: {chat[0]}\n|Chat tag: {chat[1]}\n|Chat id: {chat[2]}\n----------------")
                     choice = input("Enter server tag: ").encode()
                     sock.sendto(choice, server_address)
-                    exec(f"chat_now={sock.recv(1024).decode()}")
-                    print(f"Joined to chat('{chat_now['chat_name']}').")
+                    chat_now = sock.recv(1024).decode()
+                    exec(f"chat={chat_now}[0]")
+                    print(f"Joined to chat('{chat[0]}').")
                     
     except ConnectionResetError:
         print("Connection closed | Side: server.")
--- server.py
+++ server.py
@@ -1,7 +1,15 @@
 import socket
 import threading as t
 from random import randint
+import sqlite3
+import time
 
+database = sqlite3.connect("messenger_base.db", check_same_thread=False)
+cur = database.cursor()
+
+database.execute("create table if not exists Users(user_nickname TEXT, user_tag TEXT, user_id INTEGER, user_mail TEXT, chat_now TEXT, chats_list TEXT, creation_date TEXT)")
+database.execute("create table if not exists Chats(chat_title TEXT, chat_tag TEXT, chat_id INTEGER, chat_members TEXT, creation_date TEXT)")
+database.execute("create table if not exists Messages(author_id INTEGER, chat_id INTEGER, message_id INTEGER, message_text TEXT, creation_date TEXT)")
 
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 machine_address = ("localhost", 38385)
@@ -13,15 +21,8 @@ class Server_data():
     chats = []
     tracking = []
 
-    
-for index_chat in range(0,3):
-    chat_id = ''.join([*map(str, [randint(0,9) for _ in range(0,12)])])
-    chat_tag = ''.join(["socketchat"[randint(0,9)] for _ in range(0,5)])
-    Server_data.chats += [{"chat_name": f"Chat_{index_chat}",
-               "chat_id": chat_id,
-               "chat_tag": chat_tag,
-               "chat_members": [],
-                "chat_messages": []}]
+#chat_id = ''.join([*map(str, [randint(0,9) for _ in range(0,12)])])
+#chat_tag = ''.join(["socketchat"[randint(0,9)] for _ in range(0,5)])
 
 server_sock.bind(machine_address)
 server_sock.listen(2)
@@ -50,47 +51,63 @@ def connector_c():
         connection, addr = server_sock.accept()
         if addr != None:
             print("Connected:", addr)
-            ID_user = connection.recv(1024).decode()
-            Server_data.clients += [{"user_id": ID_user, "addr": addr, "connection": connection, "chat_now": [], "chat_list": []}]
-            recv_messages_from_client = t.Thread(target=recviving_messages_from_clients, args=[[connection, addr, ID_user, ""]]) #FORM [CONNECTION, addr, ID_user, chat]
-            recv_messages_from_client.start()
-            Server_data.tracking += [recv_messages_from_client]
+            user_info = connection.recv(1024).decode().split("↔")
+            cur.execute(f"SELECT * FROM Users WHERE user_id={user_info[2]}")
+            info = cur.fetchone()
+            if info==None:
+                #Незареєстрований клієнт | Регістрація 
+                database.execute(f'insert into Users(user_nickname, user_tag, user_id, user_mail, creation_date, chat_now, chats_list) VALUES("{user_info[0]}", "{user_info[1]}", "{user_info[2]}", "{user_info[3]}", "{user_info[4]}", "None", "");')
+                database.commit()
+                cur.execute(f"select * from Users where user_id={user_info[2]}")
+                info = cur.fetchone()
+                recv_messages_from_client = t.Thread(target=recviving_messages_from_clients, args=[info, [connection, addr]]) #FORM [CONNECTION, addr, ID_user, chat]
+                recv_messages_from_client.start()
+            else:
+                #Зареєстрований клієнт | Авторизація
+                cur.execute(f"select * from Users where user_id={user_info[2]}")
+                info = cur.fetchone()
+                recv_messages_from_client = t.Thread(target=recviving_messages_from_clients, args=[info, [connection, addr]]) #FORM [CONNECTION, addr, ID_user, chat]
+                recv_messages_from_client.start()
 
 
-def recviving_messages_from_clients(data):
+def recviving_messages_from_clients(data_user, data_connection):
     global Server_data
-    if len(data)!=0:
-        id_user = data[2]
+    if len(data_connection)!=0:
+        user_id = data_user[2]
         while 1:
-            if len(data[3])!=0:
+            if data_user[4]!="":
                 "Створення та відправка повідомлень у різні чати"
-                data[0].sendto("CODE:201".encode(), data[1])
-                exec(f"chat_id={data[3]}")
-                for chat in Server_data.chats:
-                    if chat["chat_id"] == chat_id:
-                        message_id = ''.join([*map(str, [randint(0,9) for _ in range(0,10)])])
-                        message_contain = data[0].recv(1024).decode()
-                        chat["chat_messages"] += [{"author": id_user,
-                                "message": message_contain,
-                                "is_read": [],
-                                "message_id": message_id}]
-                for message in Server_data.messages:
-                    data[0].sendto(f"100#{Server_data.chats}".encode(), data[1])
+                cur.execute("SELECT * FROM Chats;")
+                chats = [*{*cur.fetchall()}]
+                data_connection[0].sendto(f"{chats}".encode(), data_connection[1])
+                chat_id=data_user[4]
+                cur.execute(f"SELECT chat_now FROM Users WHERE user_id={data_user[2]}")
+                chat_id = cur.fetchone()[0]
+                time_now = time.localtime()
+                time_structurized = f"{time_now.tm_hour}:{time_now.tm_min}:{time_now.tm_sec}|{time_now.tm_mday}.{time_now.tm_mon}.{time_now.tm_year}"
+                message_id = ''.join([*map(str, [randint(0,9) for _ in range(0,10)])])
+                message_text = data_connection[0].recv(1024).decode()
+                database.execute(f"INSERT INTO Messages VALUES('{user_id}','{chat_id}','{message_id}','{message_text}', '{time_structurized}');")
+                database.commit()
             else:
                 "Запрос на підключення користувача до бажанного чату"
-                data[0].sendto(f"200#{Server_data.chats}".encode(), data[1])
-                chat_choice = data[0].recv(1024).decode()
-                data[3] = f"{[chat for chat in Server_data.chats if chat['chat_tag']==chat_choice][0]}"
-                data[0].sendto(data[3].encode(), data[1])
-                for client in Server_data.clients:
-                    if client["user_id"]==data[2]:
-                        for chat in Server_data.chats:
-                            if chat["chat_tag"]==chat_choice:
-                                chat["chat_members"] += [id_user]
-                                client["chat_now"] = chat["chat_id"]
-                                client["chat_list"] += [chat["chat_id"]]
-                                break
-                        
+                cur.execute("SELECT * FROM Chats;")
+                chats = cur.fetchall()
+                data_connection[0].sendto(f"{chats}".encode(), data_connection[1])
+                chat_choice = data_connection[0].recv(1024).decode()
+                choosed_chat_tag = chat_choice
+                cur.execute(f"select * from Chats where chat_tag='{choosed_chat_tag}'")
+                info_about_chat = cur.fetchall()[0]
+                cur.execute(f"select chats_list FROM Users WHERE user_id='{user_id}'")
+                chats_list = str(cur.fetchone())+str(info_about_chat[2])
+                cur.execute(f"select chat_members FROM Chats WHERE chat_id='{info_about_chat[1]}'")
+                chat_members = str(cur.fetchone()) + "|" + str(data_user[2])
+                print(chats_list, chat_members)
+                database.execute(f"UPDATE Users SET chat_now='{info_about_chat}', chats_list = '{chats_list.replace('[]', '')}' WHERE user_id='{data_user[2]}'")
+                database.commit()
+                database.execute(f"UPDATE Chats SET chat_members='{chat_members}' WHERE chat_id='{info_about_chat[2]}'")
+                database.commit()
+                data_connection[0].sendto(f"{info_about_chat}".encode(), data_connection[1])                  
     else:
         ...
 
